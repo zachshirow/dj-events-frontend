4bb0d4c3d607f5425830f41e9cef14baa3199ad1
Zachary Shirmohammadli <zachshirow@gmail.com>
2022-02-16 16:31:46 +0330

    Added the customized 404 page.

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+	return (
+		<Layout>
+			<div className={styles.error}>
+				<h1>404</h1>
+				<h4>Sorry, there is nothing here</h4>
+				<Link href="/">Go back home &#8594;</Link>
+			</div>
+		</Layout>
+	);
+}
