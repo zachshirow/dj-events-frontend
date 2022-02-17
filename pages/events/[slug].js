949707a8dd8c836b193fbba643397453c7f0e61a
Zachary Shirmohammadli <zachshirow@gmail.com>
2022-02-17 15:42:11 +0330

    Added the Single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,81 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function EventPage() {
+import { API_URL } from "@/config/index.js";
+import styles from "@/styles/Event.module.css";
+
+export default function EventPage({ evt }) {
+	function deleteEvent(e) {
+		console.log("delete");
+	}
+
 	return (
 		<Layout>
-			<Link href="/events/">← See All Events</Link>
-			<h1>My event</h1>
+			<div className={styles.event}>
+				<div className={styles.controls}>
+					<Link href={`/events/edit/${evt.id}`}>
+						<a>
+							<FaPencilAlt /> Edit Event
+						</a>
+					</Link>
+					<a href="#" className={styles.delete} onClick={deleteEvent}>
+						<FaTimes /> Delete Event
+					</a>
+				</div>
+
+				<span>
+					{evt.date} at {evt.time}
+				</span>
+				<h1>{evt.name}</h1>
+				{evt.image && (
+					<div className={styles.image}>
+						<Image src={evt.image} alt={evt.name} width={960} height={600} />
+					</div>
+				)}
+
+				<h3>Performers</h3>
+				<p>{evt.performers}</p>
+				<h3>Description</h3>
+				<p>{evt.description}</p>
+				<h3>Venue: {evt.venue}</h3>
+				<p>{evt.address}</p>
+				<Link href="/events/">
+					<a className={styles.back}>← Go Back</a>
+				</Link>
+			</div>
 		</Layout>
 	);
 }
+
+export async function getStaticPaths() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+
+	const paths = events.map((evt) => {
+		return { params: { slug: evt.slug } };
+	});
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params: { slug } }) {
+	const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+	const events = await res.json();
+
+	return { props: { evt: events[0] }, revalidate: 1 };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+// 	const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+// 	const events = await res.json();
+
+// 	return { props: { evt: events[0] } };
+// }
