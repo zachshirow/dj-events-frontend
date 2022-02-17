b4254783b34a7505a3a208e7d5f8eb363b0ba11d
Zachary Shirmohammadli <zachshirow@gmail.com>
2022-02-17 13:28:37 +0330

    Implemented the events api routes: both get events, and get event by [slug] routes.

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { events } = require("./data.json");
+
+export default function handler(req, res) {
+	const slug = req.query.slug;
+
+	const evt = events.filter((ev) => {
+		return ev.slug === slug;
+	});
+
+	if (req.method == "GET") {
+		res.status(200).json(evt);
+	} else {
+		res.setHeader("Allow", ["GET"]);
+		res.status(405).json({ message: `Method ${req.method} is not allowed.` });
+	}
+}
